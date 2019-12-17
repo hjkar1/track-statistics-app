28776ee3baae6447fb532318e2e77fc507d3b69a
hjkar1 <hjkar1@outlook.com>
2019-12-17 22:26:12 +0200

    add functions for transforming track data for visualization

--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,43 @@ export const getAuthHeader = () => {
 
   return null;
 };
+
+// Return the modes of the tracks as data for react-vis pie chart.
+
+export const getModes = tracks => {
+  const tracksInMajor = tracks.filter(track => track.mode === 1);
+  const tracksInMinor = tracks.filter(track => track.mode === 0);
+  const numberOfTracksInMajor = tracksInMajor.length;
+  const numberOfTracksInMinor = tracksInMinor.length;
+
+  return [
+    { angle: numberOfTracksInMajor, label: 'Major' },
+    { angle: numberOfTracksInMinor, label: 'Minor' }
+  ];
+};
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+const calculateAverage = values => values.reduce(reducer) / values.length;
+
+const getFeatures = (tracks, feature) => tracks.map(track => track[feature]);
+
+export const getAverages = tracks => {
+  const acousticness = getFeatures(tracks, 'acousticness');
+  const danceability = getFeatures(tracks, 'danceability');
+  const energy = getFeatures(tracks, 'energy');
+  const instrumentalness = getFeatures(tracks, 'instrumentalness');
+  const liveness = getFeatures(tracks, 'liveness');
+  const speechiness = getFeatures(tracks, 'speechiness');
+  const valence = getFeatures(tracks, 'valence');
+
+  return [
+    { y: 'acousticness', x: calculateAverage(acousticness) },
+    { y: 'danceability', x: calculateAverage(danceability) },
+    { y: 'energy', x: calculateAverage(energy) },
+    { y: 'instrumentalness', x: calculateAverage(instrumentalness) },
+    { y: 'liveness', x: calculateAverage(liveness) },
+    { y: 'speechiness', x: calculateAverage(speechiness) },
+    { y: 'valence', x: calculateAverage(valence) }
+  ];
+};
