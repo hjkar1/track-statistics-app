0a8283c840c4eb6ac6b6777baba65f5e690c841c
hjkar1 <hjkar1@outlook.com>
2019-12-17 17:29:02 +0200

    add login link

---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import Container from './Container';
+
+const clientID = process.env.REACT_APP_CLIENT_ID;
+
+// Callback URI for redirect after login.
+const callbackURI = process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_PROD_URI
+    : process.env.REACT_APP_DEV_URI;
+
+const LinkButton = styled.a`
+  background-color: green;
+  border-radius: 4px;
+  color: white;
+  margin: 1rem;
+  padding: 0.5rem;
+  text-decoration: none;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <div style={{ margin: '1rem' }}>Login with your Spotify account</div>
+      <LinkButton
+        href={`https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${callbackURI}&scope=user-read-private%20user-read-email%20user-top-read&response_type=token&state=123`}
+      >
+        Login
+      </LinkButton>
+    </Container>
+  );
+};
+
+export default Login;
