e1d47990728675b2839dfa67a54d17db76d422d8
hjkar1 <hjkar1@outlook.com>
2019-12-18 00:07:09 +0200

    refactor data fetching, add redirect in case of 401 error, add titles to charts

--- src/components/TrackData.js
+++ src/components/TrackData.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import queryString from 'query-string';
 import { Redirect } from 'react-router-dom';
-import { getUsersTopTracks, getAudioFeatures } from '../service';
+import { getAudioFeatures } from '../service';
 import PieChart from './visualization/PieChart';
 import BarChart from './visualization/BarChart';
 import { getModes, getAverages } from '../utils';
 import Container from './Container';
 
-const TrackData = ({ location }) => {
+const TrackData = ({ history, location }) => {
   const [trackData, setTrackData] = useState(null);
 
   const hash = queryString.parse(location.hash);
@@ -20,14 +20,21 @@ const TrackData = ({ location }) => {
 
   useEffect(() => {
     const fetchAudioFeatures = async () => {
-      const trackIds = await getUsersTopTracks();
-      const audioFeatures = await getAudioFeatures(trackIds);
-      setTrackData(audioFeatures);
+      const audioFeatures = await getAudioFeatures();
+
+      if (audioFeatures === 'redirect') {
+        history.push('/authorize');
+      }
+
+      if (audioFeatures) {
+        setTrackData(audioFeatures);
+      }
     };
+
     if (localStorage.getItem('authToken')) {
       fetchAudioFeatures();
     }
-  }, [hash]);
+  }, [history]);
 
   if (!localStorage.getItem('authToken') && !hash.access_token) {
     return <Redirect to="/authorize" />;
@@ -36,10 +43,12 @@ const TrackData = ({ location }) => {
   return (
     <Container>
       {trackData && (
-        <div>
+        <Fragment>
+          <div style={{ margin: '1.5rem' }}>Modes of your top tracks:</div>
           <PieChart data={getModes(trackData)} />
+          <div style={{ margin: '1.5rem' }}>Feature averages of your top tracks:</div>
           <BarChart data={getAverages(trackData)} />
-        </div>
+        </Fragment>
       )}
     </Container>
   );
