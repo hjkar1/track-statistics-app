16a8204da88b0650e72c53ddd9775ee61f18553c
hjkar1 <hjkar1@outlook.com>
2019-12-18 16:02:18 +0200

    add function for calculation of the average tempo, add comments

--- src/utils.js
+++ src/utils.js
@@ -29,20 +29,23 @@ export const getModes = tracks => {
   ];
 };
 
+// Get the averages of audio features of the tracks as data for react-vis bar chart.
+
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 const calculateAverage = values => values.reduce(reducer) / values.length;
 
-const getFeatures = (tracks, feature) => tracks.map(track => track[feature]);
+const getTracksWithFeature = (tracks, feature) =>
+  tracks.map(track => track[feature]);
 
 export const getAverages = tracks => {
-  const acousticness = getFeatures(tracks, 'acousticness');
-  const danceability = getFeatures(tracks, 'danceability');
-  const energy = getFeatures(tracks, 'energy');
-  const instrumentalness = getFeatures(tracks, 'instrumentalness');
-  const liveness = getFeatures(tracks, 'liveness');
-  const speechiness = getFeatures(tracks, 'speechiness');
-  const valence = getFeatures(tracks, 'valence');
+  const acousticness = getTracksWithFeature(tracks, 'acousticness');
+  const danceability = getTracksWithFeature(tracks, 'danceability');
+  const energy = getTracksWithFeature(tracks, 'energy');
+  const instrumentalness = getTracksWithFeature(tracks, 'instrumentalness');
+  const liveness = getTracksWithFeature(tracks, 'liveness');
+  const speechiness = getTracksWithFeature(tracks, 'speechiness');
+  const valence = getTracksWithFeature(tracks, 'valence');
 
   return [
     { y: 'acousticness', x: calculateAverage(acousticness) },
@@ -53,4 +56,12 @@ export const getAverages = tracks => {
     { y: 'speechiness', x: calculateAverage(speechiness) },
     { y: 'valence', x: calculateAverage(valence) }
   ];
+};
+
+// Get the average tempo of the tracks.
+
+export const getAverageTempo = tracks => {
+  const trackTempos = getTracksWithFeature(tracks, 'tempo');
+  const averageTempo = calculateAverage(trackTempos);
+  return Math.round(averageTempo);
 };
